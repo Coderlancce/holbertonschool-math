40135b2a5124e5203944274ad943f875fb7e32ef
Coderlancce <coderlancce@gmail.com>
2022-04-05 12:45:17 -0500

    0 task

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -12,23 +12,23 @@ void display_complex_number(complex c)
 	if (c.re == 0)
 	{
 		if (c.im < 0)
-			printf("- %.9fi\n", c.im * (-1));
+			printf("- %.9gi\n", c.im * (-1));
 		else if (c.im == 0)
 			printf("0\n");
 		else
-		  printf("%.9fi\n", c.im);
+		  printf("%.9gi\n", c.im);
 	}
 	else
 	{
 		if (c.im < 0)
-			printf("%9f - %.9fi\n", c.re, c.im * (-1));
+			printf("%.9g - %.9gi\n", c.re, c.im * (-1));
 		else if (c.im == 0)
-			printf("%.9f\n", c.re);
+			printf("%.9g\n", c.re);
 		else if (c.im == 1)
 			printf("%.9f + i\n", c.re);
 		else if (c.im == -1)
-			printf("%.9f - i\n", c.re);
+			printf("%.9g - i\n", c.re);
 		else
-			printf("%.9f + %.9fi\n", c.re, c.im);
+			printf("%.9g + %.9gi\n", c.re, c.im);
 	}
 }
